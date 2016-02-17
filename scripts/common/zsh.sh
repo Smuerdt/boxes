e346f5bf1d2d5d27331d4ce4de7c4bce84b1165f
Nico Mürdter <nicomuerdter@gmail.com>
2016-02-17 21:49:51 +0100

    added own zsh stuff

--- scripts/common/zsh.sh
+++ scripts/common/zsh.sh
@@ -3,7 +3,8 @@
 echo "Setting up zsh..."
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh > /dev/null 2>&1
-curl -s https://gist.githubusercontent.com/saelo/7614863612553a4647ae/raw/.zshrc > ~/.zshrc
+curl -s https://gist.githubusercontent.com/Smuerdt/dc14a6363f62a64ce4e1/raw/.zshrc > ~/.zshrc
+curl -s https://gist.githubusercontent.com/Smuerdt/0f339a20da4b20d67673/raw/myfrisk.zsh-theme > ~/.oh-my-zsh/custom/themes/myfrisk.zsh-theme
 
 # needs sudo to work passwordless
 sudo chsh -s $(which zsh) $(whoami)
